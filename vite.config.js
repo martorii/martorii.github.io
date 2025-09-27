c4aef04cabb9ab8c13f82f8693503fa35040e6f9
martorii <erik.martori@gmail.com>
2025-09-27 13:30:07 +0200

    jsx to js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/martorii.github.io/'
+  base: '/martorii.github.io/',
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.[jt]sx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
+  }
 })
